9b0fc8d298bb735d8b7218044e75af607d8d70e4
Théo Thoumine <theo@thoumine.de>
2023-10-24 10:49:47 +0200

    tries pathmapping

--- src/main/java/webtech/filmfriend/HelloController.java
+++ src/main/java/webtech/filmfriend/HelloController.java
@@ -1,6 +1,7 @@
 package webtech.filmfriend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,15 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         return "Greetings from filmfriend!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/user/{userid}")
+    public String user(@PathVariable int userid) {
+        return "Hello User! Your ID is " + userid + "!";
     }
 }
