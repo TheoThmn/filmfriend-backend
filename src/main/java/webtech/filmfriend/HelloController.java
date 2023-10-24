e87c19abe4da5f863b6271729caf57e807e58676
Théo Thoumine <theo@thoumine.de>
2023-10-24 10:57:08 +0200

    tries pathmapping w/ multiple paths

--- src/main/java/webtech/filmfriend/HelloController.java
+++ src/main/java/webtech/filmfriend/HelloController.java
@@ -16,8 +16,8 @@ public class HelloController {
         return "Hello World!";
     }
 
-    @GetMapping("/user/{userid}")
-    public String user(@PathVariable int userid) {
-        return "Hello User! Your ID is " + userid + "!";
+    @GetMapping("/user/{userid}/password/{password}")
+    public String user(@PathVariable int userid, @PathVariable String password) {
+        return "Hello User! Your ID is " + userid + "! Your password is " + password + "!";
     }
 }
